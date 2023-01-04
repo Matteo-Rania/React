a66bc3523831bce6f1c48f97bd76fa66a210253f
Matteo Rania <matteo.rania2014@gmail.com>
2023-01-04 09:10:21 +0100

    props-1

--- esercizi/src/app.js
+++ esercizi/src/app.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { Hello } from "./hello";
+import { Welcome } from "./welcome";
 
 
 export class App extends React.Component{
     render(){
-        return(
-            <div>
-                <Hello />
-            </div>
-        )
+        return <Welcome name="Matteo" />
     }
 }

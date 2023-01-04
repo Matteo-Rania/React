5c67ef8b7e3d3983fae364054fbecf420728a46c
Matteo Rania <matteo.rania2014@gmail.com>
2023-01-04 11:27:02 +0100

    conditionalRendering-2

--- esercizi/src/welcome.js
+++ esercizi/src/welcome.js
@@ -9,7 +9,7 @@ export class Welcome extends React.Component{
         return(
             <div>
                 <p>Welcome, {this.props.name}</p>
-                <Age age={this.props.age > 18 && this.props.age} />
+                {this.props.age ? <Age age={this.props.age} /> : false}
             </div>
         ) 
     }

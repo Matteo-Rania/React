5009fec4c8363c18deb3fad2ec25e0da81236520
Matteo Rania <matteo.rania2014@gmail.com>
2023-01-04 12:02:03 +0100

    conditionalRendering-5

--- esercizi/src/app.js
+++ esercizi/src/app.js
@@ -4,6 +4,6 @@ import { Welcome } from "./welcome";
 
 export class App extends React.Component{
     render(){
-        return <Welcome name= 'John' age={33} />
+        return <Welcome name= 'Giacomino' age={12} />
     }
 }

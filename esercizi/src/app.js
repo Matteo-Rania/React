e399c9bd3308f882e7ee14621b3b9510ff191ed2
Matteo Rania <matteo.rania2014@gmail.com>
2023-01-16 23:27:07 +0100

    styling-03

--- esercizi/src/app.js
+++ esercizi/src/app.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Login } from "./Login";
 import { Welcome } from "./Welcome";
 
 
 export class App extends React.Component{
     render(){
         return (
-            <div>
+            <div className="flex-column">
                 <Welcome name='matteo' />
+                <Login />
             </div>
         )
     }

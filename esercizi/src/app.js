1c4c0d380b47939217e6f0d16858f3aa7c94be22
Matteo Rania <matteo.rania2014@gmail.com>
2023-01-17 23:50:22 +0100

    renderProps-01

--- esercizi/src/app.js
+++ esercizi/src/app.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { Container } from "./Container";
-import { Login } from "./Login";
-import { Welcome } from "./Welcome";
+import { ToDoList } from "./TodoList";
 
 
 export class App extends React.Component{
     render(){
         return (
-            <Container title='component composition'>
-                <Welcome name='matteo' />
-                <Login />
-            </Container>
+            <ToDoList />
         )
     }
 }
--- esercizi/src/index.js
+++ esercizi/src/index.js
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
-import "./index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
